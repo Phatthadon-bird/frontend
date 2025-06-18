5bf1f0ec97bb33a960430132ab2da3204ef78222
Tayakon Intanon <tachagron1964@gmail.com>
2025-06-18 10:40:17 +0700

    v.3

--- app/page.js
+++ app/page.js
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    Mr.Phatthadon kachi
-    </>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-start', // อยู่ด้านบน
+      height: '100vh',
+      paddingTop: '20px', // ระยะห่างจากขอบบน
+      fontSize: '24px'
+    }}>
+      Mr.Phatthadon kachi
+    </div>
   );
 }
