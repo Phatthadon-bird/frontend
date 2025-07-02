9d65689b311ea9c5698182e78cf90d6a31965628
Tayakon Intanon <tachagron1964@gmail.com>
2025-07-02 11:58:30 +0700

    qa

--- app/page.js
+++ app/page.js
@@ -1,23 +1,9 @@
-import Link from 'next/link'; // นำเข้า Link จาก Next.js
-
-export default function Home() {
+// app/page.js
+export default function HomePage() {
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-    }}>
-      {/* ส่วนเนื้อหาหลัก */}
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        fontSize: '24px',
-      }}>
-        <h1>Mr.Phatthadon Kachi</h1>
-      </div>
-    </div>
-  );
+    <main>
+      <h1>ยินดีต้อนรับสู่เว็บไซต์ของเรา</h1>
+      <p>Phatthadon kachai</p>
+    </main>
+  )
 }
